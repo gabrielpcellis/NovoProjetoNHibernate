9e7f18d0959139bbe2dd611b492ba1919afad172
Gabriel Porcellis <gabrielporcellis@gmail.com>
2021-02-12 14:54:55 -0300

    Creating entities and mappers

---
+++ NovoProjetoNHibernate/Entities/Sales.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace NovoProjetoNHibernate.Entities
+{
+    class Sales
+    {
+        public virtual int Id { get; set; }
+        public virtual DateTime SaleDate { get; set; }
+        public virtual Customer Customer { get; set; }
+
+        public Sales(int id, DateTime dateSale)
+        {
+            Id = id;
+            SaleDate = dateSale;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Date:" + SaleDate);
+            sb.AppendLine("Customer" + Customer);
+            return sb.ToString();
+        }
+    }
+}
